a3a21ce335a8219deaf739590b76f9790a213629
Samuel Ivarsson <isamuel@student.chalmers.se>
2022-12-02 10:19:46 +0100

    Added TroubleToggle plugin

--- lvim/.config/lvim/lua/user/plugins.lua
+++ lvim/.config/lvim/lua/user/plugins.lua
@@ -89,6 +89,17 @@ lvim.plugins = {
             }
         end,
     },
+    {
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
     -- "MunifTanjim/nui.nvim",
     -- {
     --   "folke/noice.nvim",
