6244f273e602f5404c0a4b5d691efd188d04eb5c
Samuel Ivarsson <isamuel@student.chalmers.se>
2022-12-09 02:35:57 +0100

    Added formatters in lvim config

--- lvim/.config/lvim/lua/user/lsp/init.lua
+++ lvim/.config/lvim/lua/user/lsp/init.lua
@@ -14,9 +14,9 @@ lvim.builtin.treesitter.ensure_installed = {
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
 
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---     { command = "google_java_format", filetypes = { "java" } },
---     { command = "stylua", filetypes = { "lua" } },
---     { command = "shfmt", filetypes = { "sh", "zsh" } },
--- }
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+    { command = "google_java_format", filetypes = { "java" } },
+    { command = "stylua", filetypes = { "lua" } },
+    { command = "shfmt", filetypes = { "sh", "zsh" } },
+}
