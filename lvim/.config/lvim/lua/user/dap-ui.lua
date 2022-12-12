9440410fea416e22b5a993830f5a6a41fa87f0cf
Samuel Ivarsson <isamuel@student.chalmers.se>
2022-12-12 02:07:16 +0100

    Dap UI no longer auto opens - lvim config

--- lvim/.config/lvim/lua/user/dap-ui.lua
+++ lvim/.config/lvim/lua/user/dap-ui.lua
@@ -1,38 +1,50 @@
-local status_ok, dap_ui = pcall(require, "dapui")
-if not status_ok then
-    return
-end
-
 local config = lvim.builtin.dap.ui.config
-config.layouts =
-{
-    {
-        elements = {
-            -- { id = "repl", size = 0.01 },
-            { id = "console", size = 0.59 },
-            { id = "scopes", size = 0.1 },
-            { id = "breakpoints", size = 0.1 },
-            { id = "stacks", size = 0.1 },
-            { id = "watches", size = 0.1 },
-        },
-        size = 0.33,
-        position = "right",
-    },
-    {
-        elements = {
-            { id = "repl", size = 1 },
-        },
-        size = 0.01,
-        position = "bottom",
-    },
+config.layouts = {
+	{
+		elements = {
+			{ id = "repl", size = 0.20 },
+			-- { id = "console", size = 0.59 },
+			{ id = "scopes", size = 0.20 },
+			{ id = "breakpoints", size = 0.15 },
+			{ id = "stacks", size = 0.3 },
+			{ id = "watches", size = 0.15 },
+		},
+		size = 0.20,
+		position = "left",
+	},
+	-- {
+	-- 	elements = {
+	-- 		{ id = "repl", size = 1 },
+	-- 	},
+	-- 	size = 0.01,
+	-- 	position = "bottom",
+	-- },
 }
 config.floating = {
-    max_height = 0.9,
-    max_width = 0.5, -- Floats will be treated as percentage of your screen.
-    border = vim.g.border_chars, -- Border style. Can be 'single', 'double' or 'rounded'
-    mappings = {
-        close = { "q", "<Esc>" },
-    },
+	max_height = 0.9,
+	max_width = 0.5, -- Floats will be treated as percentage of your screen.
+	border = vim.g.border_chars, -- Border style. Can be 'single', 'double' or 'rounded'
+	mappings = {
+		close = { "q", "<Esc>" },
+	},
 }
 
 lvim.builtin.dap.ui.config = config
+lvim.builtin.dap.ui.auto_open = false
+-- vim.notify("HEEEEJ", "INFO")
+local dap = require("dap")
+dap.defaults.fallback.external_terminal = {
+	command = "kitty",
+	args = { "-e" },
+}
+-- TODO Make this work
+-- lvim.builtin.dap["on_config_done"] = function()
+-- 	vim.notify("Hjsa!", "INFO")
+-- 	vim.notify(vim.inspect(dap.defaults.fallback.terminal_win_cmd))
+-- 	dap.defaults.fallback.terminal_win_cmd = "50vsplit new"
+-- 	vim.notify(vim.inspect(dap.defaults.fallback.terminal_win_cmd))
+-- end
+-- vim.notify("Hejsan!", "INFO")
+-- print("Halloooo")
+-- dap.defaults.fallback.terminal_win_cmd = "50vsplit new"
+dap.defaults.fallback.focus_terminal = true
