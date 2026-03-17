1adf103fd1491228356a93aab471db8645553565
Samuel Ivarsson <samuel.ivarsson@gung.se>
2026-03-17 15:44:18 +0100

    nvim: disable hop

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -44,6 +44,7 @@ local plugins = {
         config = function()
             -- you can configure Hop the way you like here; see :h hop-config
             require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+            -- require 'hop'.setup { keys = 'eovxqpdygblzhckisuran' }
         end
     },
     {
